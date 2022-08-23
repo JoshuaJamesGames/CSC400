53abd76d50f120243978ff18ce093d829fc161d2
Joshua James <thecoloradothompsons@gmail.com>
2022-08-23 14:26:56 -0600

    MyType.equals implemented

--- MyType.java
+++ MyType.java
@@ -18,4 +18,24 @@ public class MyType {
         
         return String.format("Customer #%d: %s $%.02f", customerNumber, itemName, price);
     }
+
+    @Override
+    public boolean equals(Object anObject){
+        //Compares a MyType with another Object
+        if (anObject == this) {
+            return true;
+        }
+
+        if (!(anObject instanceof MyType)) {
+            return false;
+        }
+        
+        //Cast to MyType - Won't get this far if not compatible
+        MyType checkMe = (MyType) anObject;
+
+        //All attributes must match
+        return (checkMe.customerNumber == this.customerNumber && 
+                checkMe.itemName.equals(this.itemName) && 
+                checkMe.price == this.price);
+    }
 }
