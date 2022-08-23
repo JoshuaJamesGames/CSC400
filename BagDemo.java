774223ced6d1bf6efe01fdbb48ca2c67dc6c1339
Joshua James <thecoloradothompsons@gmail.com>
2022-08-23 13:56:58 -0600

    BagDemo & ShoppingCart Outlined

--- BagDemo.java
+++ BagDemo.java
@@ -1,5 +1,12 @@
 package CSC400;
 
 public class BagDemo {
-    
+    public static void main(String[] args) {
+
+        //Create a Shopping cart
+        
+        //testAddl()
+
+        //displayBag()
+    }
 }
--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,5 +1,55 @@
 package CSC400;
 
-public class ShoppingCart {
+public class ShoppingCart implements Bag{
+
+    MyType[] myBag;
+    int numberOfProducts = 0;
+    int Default_Capacity;
+
+    ShoppingCart(){
+        //
+        myBag = new MyType[1];
+        Default_Capacity = 1;
+    }
+
+    ShoppingCart(int Default_Capacity){
+        //
+        myBag = new MyType[Default_Capacity];
+        //
+        for(int i=0; i<myBag.length; i++){
+            myBag[i]=null;
+        }
+
+        this.Default_Capacity = Default_Capacity;
+    }
+    
+    @Override
+    public boolean add() {
+        //
+        
+        return false;
+    }
+
+    @Override
+    public boolean remove() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        //
+        if(numberOfProducts < Default_Capacity){
+            return false;
+        }else{
+            return true;
+        }        
+    }
+
+    @Override
+    public <T> T[] toArray() {
+        //
+        return null;
+    }
     
 }
