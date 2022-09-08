5061e6a7ad51416d8d70dd9c76f13be910ca28c6
Joshua James <thecoloradothompsons@gmail.com>
2022-09-08 11:35:42 -0600

    Comments

--- CSC400_CT4.java
+++ CSC400_CT4.java
@@ -1,13 +1,15 @@
 
 public class CSC400_CT4 {
     public static void main(String[] args) {
-        
+
+        //Test expressions from textbook
         Expression test1 = new Expression("(a+b)/(c-d)");
         Expression test2 = new Expression("a/(b-c)*d");
         Expression test3 = new Expression("a-(b/(c-d)*e+f)^g");
         Expression test4 = new Expression("(a-b*c)/(d*e^f*g+h)");
         Expression test5 = new Expression("a/b*(c+(d-e))");
 
+        //Output postfix from each test
         System.out.println(test1.getPostfix());
         System.out.println(test2.getPostfix());
         System.out.println(test3.getPostfix());
