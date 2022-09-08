786efdeadc9faa734366d1cb58077d5189852551
Joshua James <thecoloradothompsons@gmail.com>
2022-09-08 10:27:01 -0600

    Tests

--- CSC400_CT4.java
+++ CSC400_CT4.java
@@ -9,6 +9,10 @@ public class CSC400_CT4 {
         String test4 = "(a-b*c)/(d*e^f*g+h)";
         String test5 = "a/b*(c+(d-e))";
 
+        System.out.println(convertToPostfix(test1));
+        System.out.println(convertToPostfix(test2));
+        System.out.println(convertToPostfix(test3));
+        System.out.println(convertToPostfix(test4));
         System.out.println(convertToPostfix(test5));
         
 
