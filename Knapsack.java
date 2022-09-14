a222a8920eea9ea712e6981da26de3db57e18849
Joshua James <thecoloradothompsons@gmail.com>
2022-09-13 19:50:06 -0600

    Knapsack add() implemented

--- Knapsack.java
+++ Knapsack.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class Knapsack {
     
     private int size = 0;
-    private String[] contents;
+    private ArrayList<String> contents;
     private int value = 0;
     private int contentSize = 0;
 
@@ -9,16 +11,29 @@ public class Knapsack {
         this.size = size;
     }
 
-    public void add(KnapsackItem item){
+    public boolean add(KnapsackItem item){
+
+        if((contentSize + item.size) <= this.size){
+
+            contents.add(item.name);
+            value += item.value;
+            contentSize += item.size;
+
+            return true;
+
+        }else{
+            return false;
+        }
         
+
     }
 
     public int getSize(){
         return size;
     }
 
-    public String[] getContents(){
-        return contents;
+    public String getContents(){
+        return contents.toString();
     }
 
     public int getValue(){
