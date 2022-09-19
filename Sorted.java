5de4effdfd2fb9071c0558ad97044c2e70b09eb7
Joshua James <thecoloradothompsons@gmail.com>
2022-09-19 11:14:32 -0600

    ascending with Comparator

--- Sorted.java
+++ Sorted.java
@@ -1,13 +1,30 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Sorted {
-    
+
+    //ArrayList
     public static <T extends Comparable<T>> boolean ascending(ArrayList<T> checkArray){
 
         int index = 0;
 
         while(index < checkArray.size() -1){
             if(checkArray.get(index).compareTo(checkArray.get(index +1)) > 0){
+                return false;
+            } 
+            index++;           
+        }
+
+        return true;
+    }
+
+    //Generic Array with Comparator
+    public static <T> boolean ascending(T[] checkArray, Comparator<? super T> comparator){
+
+        int index = 0;
+
+        while(index < checkArray.length -1){
+            if(comparator.compare(checkArray[index], checkArray[index +1]) > 0){
                 return false;
             } 
             index++;           
