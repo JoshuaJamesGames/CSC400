a4ff3ae169061f26a29d2260c5fdd550770bc5ff
Joshua James <thecoloradothompsons@gmail.com>
2022-09-19 09:42:45 -0600

    Primitive test cases

--- CSC400_CT6.java
+++ CSC400_CT6.java
@@ -11,9 +11,19 @@ You can use your method to test whether a sort method has executed correctly.
 public class CSC400_CT6 {
     public static void main(String[] args) {
         //Create several array test cases to use with Sorted.ascending()
-        int[] testInt = new int[] {1,2,3,4,5};
 
-        System.out.println(Sorted.ascending(testInt));
+        char[] testChar = new char[]{'a','b','c','d','e'};
+        char[] testCharFalse = new char[]{'a','b','c','e','d'};
+        System.out.println("{a, b, c, d, e} is sorted: "+ Sorted.ascending(testChar));
+        System.out.println("{a, b, c, e, d} is sorted: "+ Sorted.ascending(testCharFalse));
+        
+        int[] testInt = new int[] {1,2,3,4,5};
+        int[] testIntFalse = new int[] {1,2,3,5,4};
+        System.out.println("{1, 2, 3, 4, 5} is sorted: "+Sorted.ascending(testInt));
+        System.out.println("{1, 2, 3, 5, 4} is sorted: "+Sorted.ascending(testIntFalse));
+
+        
 
     }
+
 }
