3660e69e77af884aaa53b0c51a15c46716421f27
Joshua James <thecoloradothompsons@gmail.com>
2022-09-19 09:12:51 -0600

    Comments

--- Sorted.java
+++ Sorted.java
@@ -1,6 +1,6 @@
 public class Sorted {
     
-    public <T extends Comparable<? super T>> boolean ascending(T[] checkArray){
+    public static <T extends Comparable<T>> boolean ascending(T[] checkArray){
 
         int index = 0;
 
@@ -12,5 +12,105 @@ public class Sorted {
 
         return true;
     }
+
+    //Byte
+    public static boolean ascending(byte[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+
+    //Char
+    public static boolean ascending(char[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+
+    //Double
+    public static boolean ascending(double[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+
+    //Float
+    public static boolean ascending(float[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+
+    //Int
+    public static boolean ascending(int[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+
+    //long
+    public static boolean ascending(long[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+    
+    //short
+    public static boolean ascending(short[] checkArray){
+
+        int index = 0;
+
+        while(index++ < checkArray.length){
+            if(checkArray[index] > (checkArray[index +1])){
+                return false;
+            }            
+        }
+
+        return true;
+    }
+
+
 
 }
